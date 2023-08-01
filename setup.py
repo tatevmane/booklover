ed90bdc91680c1c58f3754022ddfecb628fc39a3
Tatev Gomtsyan <90347726+tatevmane@users.noreply.github.com>
2023-07-31 21:53:54 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,1 @@
+import setuptoolsfrom setuptools import setup, find_packagessetup(    name = 'booklover',    version = '0.1.0',    author = 'Tatev Gomtsyan',     url = 'git@github.com:tatevmane/booklover.git',    author_email = 'tmg6jda@virginia.edu',    license = 'MIT')
